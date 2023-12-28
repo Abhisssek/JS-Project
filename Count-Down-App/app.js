75d18193caf1318ef09243f05d5dbc35837e15c2
Abhisek <abhisekpattanayak07@gmail.com>
2023-12-28 17:44:09 +0530

    Time: 17.15 Date: 28-12-2023

--- Count-Down-App/app.js
+++ Count-Down-App/app.js
@@ -1,27 +1,38 @@
-const endDate= "29 December 2023 12:00 PM"
+// Set the end date for the countdown timer
+const endDate = "29 December 2023 12:00 PM";
 
-document.getElementById("end-date").innerText= endDate;
-const inputs= document.querySelectorAll("input");
+// Display the end date on the HTML element with the ID "end-date"
+document.getElementById("end-date").innerText = endDate;
 
+// Select all input elements on the page
+const inputs = document.querySelectorAll("input");
 
-function clock(){
-    const end= new Date(endDate);
-    const now =new Date();
-    const diff= (end-now)/1000;
+// Function to update the countdown clock
+function clock() {
+    // Create Date objects for the end date and the current date and time
+    const end = new Date(endDate);
+    const now = new Date();
 
-    if(diff<0) return;
+    // Calculate the time difference in seconds between the end date and the current date
+    const diff = (end - now) / 1000;
 
-    inputs[0].value = Math.floor(diff/3600/24);
-    inputs[1].value = Math.floor(diff/3600)%24;
-    inputs[2].value = Math.floor(diff/60)%60;
-    inputs[3].value = Math.floor(diff)%60;
+    // If the countdown has reached or passed the end date, return and stop updating
+    if (diff < 0) return;
 
-   
+    // Update the input fields with the remaining days, hours, minutes, and seconds
+    inputs[0].value = Math.floor(diff / 3600 / 24);       // Days
+    inputs[1].value = Math.floor(diff / 3600) % 24;        // Hours
+    inputs[2].value = Math.floor(diff / 60) % 60;          // Minutes
+    inputs[3].value = Math.floor(diff) % 60;               // Seconds
 }
 
-clock()
-console.log(inputs)
+// Call the clock function initially to set the initial countdown values
+clock();
 
+// Log the input elements to the console for debugging or inspection
+console.log(inputs);
+
+// Update the countdown clock every second using setInterval
 setInterval(() => {
-    clock()
+    clock();
 }, 1000);
